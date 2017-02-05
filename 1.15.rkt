444d26197afd4b3193dd7dd497acecc8facc58e6
u2 <zhangyaning1985@gmail.com>
2017-02-05 22:05:39 +0800

    1.15

---
+++ 1.15.rkt
@@ -0,0 +1,14 @@
+(define (cube x) (* x x x))
+(define (p x)
+        (display x) (newline)
+        (- (* 3 x) (* 4 (cube x)))
+)
+(define (sine angle)
+   (if (not (> (abs angle) 0.1))
+       angle
+       (p (sine (/ angle 3.0)))))
+
+
+ (sine 12.15)
+ ;;; 5 times
+ ;;; O(log a)
