b40e65c8a21ed58d42d8095837e88c347f4e2555
u2 <zhangyaning1985@gmail.com>
2017-02-13 09:50:55 +0800

    1.22

---
+++ 1.22.rkt
@@ -0,0 +1,69 @@
+(#%require (only racket/base current-milliseconds))
+(define (runtime) (current-milliseconds))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) 
+                       start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) 
+         n)
+        ((divides? test-divisor n) 
+         test-divisor)
+        (else (find-divisor 
+               n 
+               (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (square x) (* x x))
+
+(define (search-for-primes from n)
+  (cond ((> n 0)
+      (if (prime? from)
+          (and
+           (timed-prime-test from)
+           (search-for-primes (next-odd from) (- n 1))
+           )
+          (search-for-primes (next-odd from) n)))))
+
+(define (continue-primes n count)
+    (cond ((= count 0)
+            (display "are primes."))
+          ((prime? n)
+            (display n)
+            (newline)
+            (continue-primes (next-odd n) (- count 1)))
+          (else
+            (continue-primes (next-odd n) count))))
+
+(define (next-odd n)
+  (if (odd? n)
+      (+ n 2)
+      (+ n 1)))
+
+
+(search-for-primes 1000 3)
+(search-for-primes 10000 3)
+(search-for-primes 1000000 3)
+(search-for-primes 10000000 3)
+(search-for-primes 100000000 3)
+(search-for-primes 1000000000 3)
+(search-for-primes 10000000000 3)
+
+
